b26c43e6d365389442258a9162a621113944e757
Karen Costa <karen.pcosta@gmail.com>
2022-11-26 22:10:41 -0300

    Add renderMenu function to display menu items

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-import menuArray from "./datajs";
+import menuArray from "./data.js";
+
+function renderMenu() {
+  let menuItem = "";
+  for (let item of menuArray) {
+    menuItem += `<div class="menu-item">
+        <img src="./images/${item.image}" alt="" />
+        <div class="item-info">
+          <h4>${item.name}</h4>
+          <p class="ingredients">${item.ingredients}</p>
+          <span class="price">$${item.price}</span>
+        </div>
+        <div class="item-button">
+          <button><i class="fa-regular fa-plus"></i></button>
+        </div>
+      </div>`;
+  }
+  document.getElementById("menu-container").innerHTML = menuItem;
+}
+
+renderMenu();
